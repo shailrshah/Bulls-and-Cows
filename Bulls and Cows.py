b3dd702a4fd0f8d6d156da155f36b8eb86817e9f
shailrshah <computerexpert94@gmail.com>
2016-06-04 08:22:30 +0530

    You can now guess 1-10 digit numbers

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -1,8 +1,48 @@
 import random
 import linecache
+
+DIGITS = 3
+
+
+def unique_values(n):
+    s = [0]*10
+    while n:
+        s[n % 10] += 1
+        if s[n % 10] > 1:
+            return False
+        n //= 10
+    return True
+
+
+def get_counts(x, ans):
+    bulls = 0
+    cows = 0
+    for i in range(0, len(str(x))):
+        user_digit = x // 10 ** i % 10
+        for j in range(0, len(str(ans))):
+            ans_digit = ans // 10 ** j % 10
+            if ans_digit == user_digit:
+                if i != j:
+                    cows += 1
+                if i == j:
+                    bulls += 1
+    return [bulls, cows]
+
+
+def no_of_lines(n):
+    # Number of different 4 digit integers with no digit repeated = 9*9*8*7 = 4536
+    mul = 1
+    for i in range(1, n+1):
+        if i == 1:
+            mul *= 9
+        else:
+            mul *= (9-i+2)
+    return mul
+
+
 print("Welcome to Shail's Bulls and Cows game!")
-print("\nThe objective of the game is to guess a 4-digit secret number, in which all digits are different.")
-print("If your digits are in the right place, then they are bulls")
+print("\nThe goal is to guess an "+str(DIGITS)+"-digit secret number, in which all digits are different.")
+print("If your digits are in the right place, then they are bulls.")
 print("If your digits are there in the secret number but not in the right place, they are cows.")
 print("\nFor example, If secret word is 4871 and your guess is 1834, then there is 1 bull(8) and 2 cows(1 and 4).")
 
@@ -10,9 +50,8 @@ print("\nFor example, If secret word is 4871 and your guess is 1834, then there 
 play_again = True
 while play_again is True:
     print("\n-------------Begin!-------------")
-    # Number of different 4 digit integers with no digit repeated = 9*9*8*7 = 4536
-    ans = int(linecache.getline('random_numbers.txt', random.randrange(0, 4536)))
-    # print("Cheat: " + str(ans))
+    ans = int(linecache.getline('random_numbers'+str(DIGITS)+'.txt', random.randrange(0, no_of_lines(DIGITS))))
+    print("Cheat: " + str(ans))
     end = False
     prev_no = []
     bulls = []
@@ -22,32 +61,29 @@ while play_again is True:
     count = 0
     while end is not True:
 
-        x = int(input("\nGuess the 4 digit number: "))
-        if len(str(x)) is not 4:
-            print("The number should be of 4 digits.")
+        x = int(input("\nGuess the "+str(DIGITS)+" digit number: "))
+        if unique_values(x) is False or len(str(x)) is not DIGITS:
+            print("The number should have "+str(DIGITS)+" unique digits.")
             continue
 
         prev_no.append(x)
-        bulls.append(0)
-        cows.append(0)
-        for i in range(0, len(str(x))):
-            user_digit = x // 10 ** i % 10
-            for j in range(0, len(str(x))):
-                ans_digit = ans // 10 ** j % 10
-                if ans_digit == user_digit:
-                    if i != j:
-                        cows[count] += 1
-                    if i == j:
-                        bulls[count] += 1
-        if bulls[count] is 4:
+        [b, c] = get_counts(x, ans)
+        bulls.append(b)
+        cows.append(c)
+
+        if bulls[count] is DIGITS:
             end = True
             print("Success! The answer was indeed "+str(ans)+"! You did it in "+str(count+1)+" steps.")
             resp = input("Do you want to play again? (Y/N): ")
             if resp == "N":
                 play_again = False
-                print("Thanks for playing Shail's Bulls and Cows!")
+                print("\nThanks for playing Shail's Bulls and Cows!")
         else:
             count += 1
             for i in range(0, count):
                 print("n = "+str(prev_no[i])+"\tbulls = "+str(bulls[i])+"\tcows = "+str(cows[i]))
             print("Try again! (Guess Count: " + str(count)+")")
+            if ans > x:
+                print("Hint: The secret number is larger than your guess!")
+            else:
+                print("Hint: The secret number is smaller than your guess!")
--- Gen_Rand.py
+++ Gen_Rand.py
@@ -1,12 +1,96 @@
 # Generate 4-digit random numbers, such that all 4 digits are unique
 from itertools import permutations
 
-result = [
+result2 = [
+    a * 10 + b * 1
+    for a, b in permutations(range(10), 2)
+    if a != 0
+]
+fh = open("random_numbers2.txt", "w")
+for i in result2:
+    print(i, file=fh)
+
+result3 = [
+    a * 100 + b * 10 + c * 1
+    for a, b, c in permutations(range(10), 3)
+    if a != 0
+]
+fh = open("random_numbers3.txt", "w")
+for i in result3:
+    print(i, file=fh)
+
+result4 = [
     a * 1000 + b * 100 + c * 10 + d * 1
     for a, b, c, d in permutations(range(10), 4)
     if a != 0
 ]
 
-fh = open("random_numbers.txt", "w")
-for i in result:
+fh = open("random_numbers4.txt", "w")
+for i in result4:
     print(i, file=fh)
+
+result5 = [
+    a * 10000 + b * 1000 + c * 100 + d * 10 + e * 1
+    for a, b, c, d, e in permutations(range(10), 5)
+    if a != 0
+]
+
+fh = open("random_numbers5.txt", "w")
+for i in result5:
+    print(i, file=fh)
+
+result6 = [
+    a * 100000 + b * 10000 + c * 1000 + d * 100 + e * 10 + f * 1
+    for a, b, c, d, e, f in permutations(range(10), 6)
+    if a != 0
+]
+
+fh = open("random_numbers6.txt", "w")
+for i in result6:
+    print(i, file=fh)
+
+
+result7 = [
+    a * 1000000 + b * 100000 + c * 10000 + d * 1000 + e * 100 + f * 10 + g * 1
+    for a, b, c, d, e, f, g in permutations(range(10), 7)
+    if a != 0
+]
+
+fh = open("random_numbers7.txt", "w")
+for i in result7:
+    print(i, file=fh)
+
+result8 = [
+    a * 10000000 + b * 1000000 + c * 100000 + d * 10000 + e * 1000 + f * 100 + g * 10 + h * 1
+    for a, b, c, d, e, f, g, h in permutations(range(10), 8)
+    if a != 0
+]
+
+fh = open("random_numbers8.txt", "w")
+for i in result8:
+    print(i, file=fh)
+
+result9 = [
+    a * 100000000 + b * 10000000 + c * 1000000 + d * 100000 + e * 10000 + f * 1000 + g * 100 + h * 10 + i * 1
+    for a, b, c, d, e, f, g, h, i in permutations(range(10), 9)
+    if a != 0
+]
+
+fh = open("random_numbers9.txt", "w")
+for i in result9:
+    print(i, file=fh)
+
+
+result10 = [
+    a * 1000000000 + b * 100000000 + c * 10000000 + d * 1000000 + e * 100000 + f * 10000 + g * 1000 + h * 100 + i * 10 + j
+    for a, b, c, d, e, f, g, h, i, j in permutations(range(10), 10)
+    if a != 0
+]
+
+fh = open("random_numbers10.txt", "w")
+for i in result10:
+    print(i, file=fh)
+
+
+
+
